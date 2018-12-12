b3e74e36669f9111055ac0107b6883a868186e86
xoriy <kerdoudtahar@gmail.com>
2018-12-12 19:46:58 +0100

    initial commit

---
+++ src/main/java/com/properties/app/main/SpringBindingPropertiesApplication.java
@@ -0,0 +1,14 @@
+package com.properties.app.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBindingPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBindingPropertiesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/properties/app/main/SpringBindingPropertiesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.properties.app.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBindingPropertiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
